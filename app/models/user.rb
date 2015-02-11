48f16a79300e8886918e9111630c208e06b55289
Benjamin Shyong <ben.shyong@gmail.com>
2015-02-11 00:45:25 -0800

    auth tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,17 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: { case_sensitive: false }, allow_nil: true
   validates :username, uniqueness: { case_sensitive: false }, presence: true
+  validates :auth_token, uniqueness: true
 
   # before_create :set_auth_token
   #
   # private
-  #   def set_auth_token
-  #     return if auth_token.present?
-  #     self.auth_token = generate_auth_token
-  #   end
-  #
-  #   def generate_auth_token
-  #     SecureRandom.uuid.gsub(/\-/,'')
-  #   end
+    def set_auth_token
+      return if auth_token.present?
+      self.auth_token = generate_auth_token
+    end
+
+    def generate_auth_token
+      SecureRandom.uuid.gsub(/\-/,'')
+    end
 end
