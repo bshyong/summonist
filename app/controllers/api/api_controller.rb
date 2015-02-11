9fefaac3af1de7d7e66e7085720d9a9e47adb5bc
Benjamin Shyong <ben.shyong@gmail.com>
2015-02-11 02:18:49 -0800

    sessions and specs

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,25 +1,34 @@
 class Api::ApiController < ApplicationController
-  private
 
-  def authenticate
-    authenticate_token || render_unauthorized
+  def current_user
+    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
 
-  def authenticate_token
-    authenticate_or_request_with_http_token do |token, options|
-      @current_user = User.find_by(auth_token: token)
-    end
-    # api_key = request.headers['X-Api-Key']
-    # @user = User.where(api_key: api_key).first if api_key
-    #
-    # unless @user
-    #   head status: :unauthorized
-    #   return false
-    # end
+  def authenticate_with_token!
+    render json: { errors: "Not authenticated" }, status: :unauthorized unless current_user.present?
   end
 
-  def render_unauthorized
-    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
-    render json: 'Bad credentials', status: 401
-  end
+  # private
+  #
+  # def authenticate
+  #   authenticate_token || render_unauthorized
+  # end
+  #
+  # def authenticate_token
+  #   authenticate_or_request_with_http_token do |token, options|
+  #     @current_user = User.find_by(auth_token: token)
+  #   end
+  #   # api_key = request.headers['X-Api-Key']
+  #   # @user = User.where(api_key: api_key).first if api_key
+  #   #
+  #   # unless @user
+  #   #   head status: :unauthorized
+  #   #   return false
+  #   # end
+  # end
+  #
+  # def render_unauthorized
+  #   self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+  #   render json: 'Bad credentials', status: 401
+  # end
 end
